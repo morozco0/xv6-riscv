15f25b325568fd3b5e50a0ed00994591fc88cae0
Matias Orozco <morozco@alumnos.uai.cl>
2023-11-22 19:19:36 -0300

    Programa de prueba

--- user/evilnull.c
+++ user/evilnull.c
@@ -11,7 +11,8 @@ int main(int argc, char *argv[]) {
     a = 5;
     pi = &a; // pi points to a
     b = *pi; // b is now 5
-    
+
+    printf("Values before NULL pointer dereference:\n");
     printf("NULL Pointer Value: %p\n", *pi);
     printf("a Pointer Value: %p\n", a);
     printf("b Pointer Value: %p\n", b);
@@ -19,9 +20,11 @@ int main(int argc, char *argv[]) {
     pi = NULL;
     c = *pi; // this is a NULL pointer dereference
 
+    printf("\nValues after NULL pointer dereference:\n");
     printf("NULL Pointer Value: %p\n", *pi);
     printf("a Pointer Value: %p\n", a);
     printf("b Pointer Value: %p\n", b);
     printf("c Pointer Value: %p\n", c);
+    
     exit(0);
 }
