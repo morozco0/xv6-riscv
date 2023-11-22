39d42a40e93a818cf7f63bf2e80b9b6f491b9dca
Matias Orozco <morozco@alumnos.uai.cl>
2023-11-22 19:42:11 -0300

    implementacion argv

--- user/yosoytupadre.c
+++ user/yosoytupadre.c
@@ -2,21 +2,17 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-int main(void) {
+int main(int argc, char *argv[]) {
+
+	int value = atoi(argv[1]);
 	 
 	printf("CON GETPID(): El proceso actual tiene PID %d.\n", getpid());	
 	
 	printf("CON GETPPID(): Yo soy tu padre - dijo el proceso %d\n", getppid());
 
-	printf("CON GETANCESTOR(int n)\n");
+	printf("CON GETANCESTOR(int n):\n");
 
-	printf("n = 0. PID: %d\n", getancestor(0));
-
-	printf("n = 1. PID: %d\n", getancestor(1));
-
-	printf("n = 2. PID: %d\n", getancestor(2));
-
-	printf("n = 3. PID: %d\n", getancestor(3));
+	printf("n = %d. PID: %d\n", value, getancestor(value));
 
 	exit(0);
 
