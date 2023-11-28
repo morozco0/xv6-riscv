8a5ababaf5bfad001cfdb75fe44d42bf57d2fb28
Matias Orozco <morozco@alumnos.uai.cl>
2023-11-28 16:15:24 -0300

    chmod

--- user/chmod.c
+++ user/chmod.c
@@ -8,28 +8,14 @@ int main(int argc, char *argv[]) {
     const char *file = argv[1];
     int mode = atoi(argv[2]);
 
-      // Crear un archivo de prueba
-    int fd = open(file, O_CREATE);
-
     // Cambiar el modo del archivo a lectura y escritura
-    int ret = chmod(file, mode); // Permisos: lectura (1) + escritura (2) = 3
+    int ret = chmod(file, mode); // Permisos: lectura (1), escritura (2) 
     if (ret < 0) {
-        printf("Error al cambiar el modo del archivo: %d\n", ret);
+        printf("Error al cambiar el modo del archivo");
         exit(1);
     }
 
-
-    // Abrir el archivo en modo lectura
-    fd = open(file, O_WRONLY);
-    if (fd < 0) {
-        printf("Error al abrir el archivo: %d\n", fd);
-        exit(1);
-    }
-
-    printf("El archivo se abrió correctamente en modo lectura\n");
-
-    // Cerrar el archivo
-    close(fd);
+    printf("Se han cambiado los permisos del archivo\n");
 
     exit(0);
 }
